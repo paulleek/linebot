5bfd64cdfc8f7a6a9ae045982474da384bdd0345
paulleek <35246381+paulleek@users.noreply.github.com>
2019-02-14 13:42:51 +0800

    Update templatebtn.php

--- templatebtn.php
+++ templatebtn.php
@@ -48,7 +48,8 @@
 			
   	//fwrite($myfile, "\xEF\xBB\xBF".json_encode($response)); //在字串前面加上\xEF\xBB\xBF轉成utf8格式
   	$header[] = "Content-Type: application/json";
-  	$header[] = "Authorization: Bearer gd1gyH+Pc5TROu9ku5u/5tDvFnffsU8nXU69zXuhTgE0dIS5nVGmx9Js8PwijeUqgFuwWXzyJ14/N5FUmp/UXsmSJbUsxMGA6AW1gozlf6cbEgSGLiC02BEaRa5wUSqE7df8FOANP1WjPW8Mh/TgtwdB04t89/1O/w1cDnyilFU=";
+  	$header[] = "Authorization: Bearer 8k1z0DBUiRirduPeUgAs1hWfBYRAcngiBEW0UypVLBypj2KnUPCgtXsb8y0Mni3UkGcbZoEGuskctssFx5QfF4T6vjyPvpmVTVCWnzcdVkNvr5k2WzqmDXrNYPlZcnpvqgOJYLFBj145BRaW0K2z/wdB04t89/1O/w1cDnyilFU=
+";
   	$ch = curl_init("https://api.line.me/v2/bot/message/reply");
   	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
   	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));                                                                  
